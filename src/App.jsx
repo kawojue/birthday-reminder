2420e3864c819e4054f65fcfd382b516a87a5576
Raheem Kawojue <kawojue08@gmail.com>
2022-10-29 15:59:03 +0100

    Done :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import List from './components/List'
-import data from '../data'
+import data from './data'
 
 function App() {
   const [people, setPeople] = useState(data)
 
   return (
     <div className="card">
-      <h2>{people.length}</h2>
+      <h2 className="text-2xl font-medium capitalize text-pry-clr-1">
+        {`${people.length} birthday${people.length > 1 ? 's' : ''} today`}
+      </h2>
       <List data={people} />
+      <button onClick={() => setPeople([])}
+        className="w-full py-1 px-2 bg-pry-clr-0 text-white text-xl tracking-wider rounded-md">
+        Clear All
+      </button>
     </div>
   )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,11 +2,16 @@
 
 const List = ({ data }) => {
     return (
-        <section>
+        <section className="flex flex-col gap-5 my-5">
             {data.map(people => (
-                <div key={people.id}>
-                    <img src={people.image} />
-                </div>
+                <article key={people.id} className="flex gap-6 items-center">
+
+                    <img src={people.image} className="rounded-full w-[90px] h-[90px] object-cover" />
+                    <div className="flex flex-col gap-2 font-medium">
+                        <h4 className="text-pry-clr-1">{people.name}</h4>
+                        <p className="text-pry-clr-2">{people.age} years</p>
+                    </div>
+                </article>
             ))}
         </section>
     )
